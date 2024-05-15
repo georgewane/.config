2cee4915d7125813cb502101107be1681e1584a0
George Christopher <georgechristopher38@gmail.com>
2024-05-15 21:51:52 +0530

    set lazy loading to false for vimtex

--- nvim/lua/neotex/plugins/vimtex.lua
+++ nvim/lua/neotex/plugins/vimtex.lua
@@ -1,5 +1,6 @@
 return {
   "lervag/vimtex",
+  lazy = false,
   init = function()
     -- vim.g['vimtex_view_method'] = 'zathura'     -- main variant with xdotool (requires X11; not compatible with wayland)
     vim.g['vimtex_view_method'] = 'zathura_simple' -- for variant without xdotool to avoid errors in wayland
